c7af9c790318a744afca0a482a5046a309646c86
jmejia31 <javiermejia3112@gmail.com>
2024-06-19 18:11:32 -0600

    gg

--- routes/web.php
+++ routes/web.php
@@ -28,20 +28,12 @@ Route::group(['middleware' => 'auth'], function () {
 		Route::get('upgrade', ['as' => 'pages.upgrade', 'uses' => 'App\Http\Controllers\PageController@upgrade']);
 });
 
-
+// ESTAS SON LAS RUTAS DEL MODULO DE USERS
 Route::group(['middleware' => 'auth'], function () {
-    // ESTAS SON LAS RUTAS DEL MODULO DE USERS
     Route::resource('users', UserController::class);
     Route::get('users/view', [UserController::class, 'show'])->name('users.view');
-    // Ruta para mostrar el formulario de edición de un usuario específico
     Route::put('user/{user}', [UserController::class, 'update'])->name('user.update');
-    // Ruta para la eliminacion de un usuario específico
     Route::get('users/{userId}/delete', [UserController::class, 'destroy']);
-
-
-
-    //Route::get('perfil', ['as' => 'profile.index', 'uses' => 'App\Http\Controllers\ProfileController@index']);
-    // Route::resource('profile', ProfileController::class);
 });
 
 // ESTAS SON LAS RUTAS DEL MODULO DE PROFILE
@@ -59,22 +51,17 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('role/{roleId}/delete', [RoleController::class, 'destroy']);
     Route::get('role/{roleId}/give-permissions', [RoleController::class, 'addPermissionToRole']);
     Route::put('role/{roleId}/give-permissions', [RoleController::class, 'givePermissionToRole']);
-
-    //SEGUNDA FORMA DE CREAR RUTAS RESOURCES Y SIMPLES mas larga
-    //Route::resource('role', App\Http\Controllers\RoleController::class);
-    //Route::get('role/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
-
 });
 
 //ESTAS SON LAS RUTAS DE PERMISSIONS
 Route::group(['middleware' => 'auth'], function () {
-    Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+    Route::resource('permissions', PermissionController::class);
     Route::get('permissions/view', [PermissionController::class, 'show'])->name('permissions.view');
-    Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
+    Route::get('permissions/{permissionId}/delete', [PermissionController::class, 'destroy']);
+    //SEGUNDA FORMA DE CREAR RUTAS RESOURCES Y SIMPLES mas largo
+    // Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+    //Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
 
-    //SEGUNDA FORMA DE CREAR RUTAS RESOURCES Y SIMPLES mas cortos
-    //Route::resource('permissions', PermissionController::class);
-    //Route::get('permissions/{permissionId}/delete', [PermissionController::class, 'destroy']);
 });
 
 
